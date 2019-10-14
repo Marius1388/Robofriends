079cbbc89446bcfb506c4c48880be3bbaabf1e5e
Marius1388 <mariuscristian.bancila@gmail.com>
2019-10-14 10:10:27 +0300

    Updates

---
+++ precache-manifest.1585e1eae0fdcb0d2a64e8449f40930e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b0d083b744dc0d3927d5a9ff69a976b",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "d573bf4d26100c99e31f",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8f1bc0efdab4682b4c67",
+    "url": "/Robofriends/static/css/main.b0d0f684.chunk.css"
+  },
+  {
+    "revision": "d573bf4d26100c99e31f",
+    "url": "/Robofriends/static/js/2.485724c4.chunk.js"
+  },
+  {
+    "revision": "8f1bc0efdab4682b4c67",
+    "url": "/Robofriends/static/js/main.73be996a.chunk.js"
+  },
+  {
+    "revision": "d385bf62d83d3c69a145",
+    "url": "/Robofriends/static/js/runtime-main.4884d92a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
